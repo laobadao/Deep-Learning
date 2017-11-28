38225960a4edb0eddc4ed26ad8397421430d98ce
2296616900@qq.com <2296616900@qq.com>
2017-11-28 15:09:54 +0800

    vectorization

--- homework/work_2_2.py
+++ homework/work_2_2.py
@@ -45,6 +45,24 @@ def for_outer(x1,x2):
 	print("outer=",np.shape(outer),"\n",outer,"\nrun time=",str(1000*(toc-tic)),"ms")
 	return outer
 
+def classic_ele(x1,x2):
+	"""
+	CLASSIC ELEMENTWISE IMPLEMENTATION
+	np.multiply  逐元相乘 得到 n 行 1 列的矩阵
+	
+	x1 = np.array([2,3,4,5,6,7,8,9])
+	x2 = np.array([4,3,6,5,8,7,8,2])
+	result:[  8.   9.  24.  25.  48.  49.  64.  18.] 
+	"""
+	tic = time.time()
+	mul = np.zeros(len(x1))
+	for i in range(len(x1)):
+		mul[i] = x1[i]*x2[i]
+	toc = time.time()
+
+	print("classic_element=",np.shape(mul),"\n",mul,"\nrun time=",str(1000*(toc-tic)),"ms")
+	return 
+
 # def element_mul(x1,x2):
 
 if __name__ == '__main__':
@@ -55,5 +73,8 @@ if __name__ == '__main__':
 	for_dot(x1,x2)
 	# dot= 245 run time= 0.0 ms
 	for_outer(x1,x2)
+	classic_ele(x1,x2)
+	# classic_element= (8,) 
+ 	# [  8.   9.  24.  25.  48.  49.  64.  18.] 
 	
 
