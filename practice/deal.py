05686f1a919a2ad33427fbed6ba45e6fc8b13e35
2296616900@qq.com <2296616900@qq.com>
2018-01-11 09:16:55 +0800

    prepare

---
+++ practice/deal.py
@@ -0,0 +1,65 @@
+import os
+import re
+
+# 去除空行
+def delblankline(infile, outfile):
+    """ Delete blanklines of infile """
+    infp = open(infile, "r", encoding='utf-8')
+    outfp = open(infile+outfile, "w", encoding='utf-8')
+    lines = infp.readlines()
+    for li in lines:
+        if li.split():
+            outfp.writelines(li)
+    infp.close()
+    outfp.close()
+
+# 添加中文逗号
+def addPunctuation(infile, outfile):
+    infp = open(infile, "r", encoding='utf-8')
+    outfp = open(infile+outfile, "w", encoding='utf-8')
+    lines = infp.readlines()
+    i = 0
+    j = 0
+    for li in lines:
+        if j == 4*i+2:
+            outfp.writelines(li+"，")
+            i = i + 1
+        j = j+1
+    infp.close()
+    outfp.close()
+
+
+# 添加英文文逗号
+def addengPunctuation(infile, outfile):
+    infp = open(infile, "r", encoding='utf-8')
+    outfp = open(infile+outfile, "w", encoding='utf-8')
+    lines = infp.readlines()
+    i = 0
+    j = 0
+    for li in lines:
+        if j == 4*i+3:
+            outfp.writelines(li)
+            i = i + 1
+        j = j+1
+    infp.close()
+    outfp.close()
+
+# 去除换行
+def deleteln(infile, outfile):
+    """ Delete blanklines of infile """
+    infp = open(infile, "r", encoding='utf-8')
+    outfp = open(infile+outfile, "w", encoding='utf-8')
+    lines = infp.readlines()
+    for li in lines:
+        li = li.strip('\n')
+        outfp.writelines(li)
+    infp.close()
+    outfp.close()
+
+# 调用示例
+if __name__ == "__main__":
+    # delblankline("4_4.txt", "nonull.txt")
+    #  addPunctuation("4_4.txt", "nonull.txt")
+     addengPunctuation("4_4.txt", "eng_notnull.txt")
+    # deleteln("4_4nonull.txt","chinese.txt")
+     deleteln("4_4eng.txt","english.txt")
