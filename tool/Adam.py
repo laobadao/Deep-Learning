71c9f716ef018690a37123d07dbc4b9c1cba6525
2296616900@qq.com <2296616900@qq.com>
2018-03-29 11:26:20 +0800

    adam

---
+++ tool/Adam.py
@@ -0,0 +1,196 @@
+import numpy as np
+
+def random_mini_batches_test_case():
+    np.random.seed(1)
+    mini_batch_size = 64
+    X = np.random.randn(12288, 148)
+    Y = np.random.randn(1, 148) < 0.5
+    return X, Y, mini_batch_size
+
+def initialize_velocity_test_case():
+    np.random.seed(1)
+    W1 = np.random.randn(2,3)
+    b1 = np.random.randn(2,1)
+    W2 = np.random.randn(3,3)
+    b2 = np.random.randn(3,1)
+    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
+    return parameters
+
+def update_parameters_with_momentum_test_case():
+    np.random.seed(1)
+    W1 = np.random.randn(2,3)
+    b1 = np.random.randn(2,1)
+    W2 = np.random.randn(3,3)
+    b2 = np.random.randn(3,1)
+
+    dW1 = np.random.randn(2,3)
+    db1 = np.random.randn(2,1)
+    dW2 = np.random.randn(3,3)
+    db2 = np.random.randn(3,1)
+    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
+    grads = {"dW1": dW1, "db1": db1, "dW2": dW2, "db2": db2}
+    v = {'dW1': np.array([[ 0.,  0.,  0.],
+        [ 0.,  0.,  0.]]), 'dW2': np.array([[ 0.,  0.,  0.],
+        [ 0.,  0.,  0.],
+        [ 0.,  0.,  0.]]), 'db1': np.array([[ 0.],
+        [ 0.]]), 'db2': np.array([[ 0.],
+        [ 0.],
+        [ 0.]])}
+    return parameters, grads, v
+    
+def initialize_adam_test_case():
+    np.random.seed(1)
+    W1 = np.random.randn(2,3)
+    b1 = np.random.randn(2,1)
+    W2 = np.random.randn(3,3)
+    b2 = np.random.randn(3,1)
+    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
+    return parameters
+
+def update_parameters_with_adam_test_case():
+    np.random.seed(1)
+    v, s = ({'dW1': np.array([[ 0.,  0.,  0.],
+         [ 0.,  0.,  0.]]), 'dW2': np.array([[ 0.,  0.,  0.],
+         [ 0.,  0.,  0.],
+         [ 0.,  0.,  0.]]), 'db1': np.array([[ 0.],
+         [ 0.]]), 'db2': np.array([[ 0.],
+         [ 0.],
+         [ 0.]])}, {'dW1': np.array([[ 0.,  0.,  0.],
+         [ 0.,  0.,  0.]]), 'dW2': np.array([[ 0.,  0.,  0.],
+         [ 0.,  0.,  0.],
+         [ 0.,  0.,  0.]]), 'db1': np.array([[ 0.],
+         [ 0.]]), 'db2': np.array([[ 0.],
+         [ 0.],
+         [ 0.]])})
+    W1 = np.random.randn(2,3)
+    b1 = np.random.randn(2,1)
+    W2 = np.random.randn(3,3)
+    b2 = np.random.randn(3,1)
+
+    dW1 = np.random.randn(2,3)
+    db1 = np.random.randn(2,1)
+    dW2 = np.random.randn(3,3)
+    db2 = np.random.randn(3,1)
+    
+    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
+    grads = {"dW1": dW1, "db1": db1, "dW2": dW2, "db2": db2}
+    
+    return parameters, grads, v, s
+    
+
+def update_parameters_with_gd_test_case():
+    np.random.seed(1)
+    learning_rate = 0.01
+    W1 = np.random.randn(2,3)
+    b1 = np.random.randn(2,1)
+    W2 = np.random.randn(3,3)
+    b2 = np.random.randn(3,1)
+
+    dW1 = np.random.randn(2,3)
+    db1 = np.random.randn(2,1)
+    dW2 = np.random.randn(3,3)
+    db2 = np.random.randn(3,1)
+    
+    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
+    grads = {"dW1": dW1, "db1": db1, "dW2": dW2, "db2": db2}
+    
+    return parameters, grads, learning_rate
+
+def initialize_adam(parameters) :
+    """
+    Initializes v and s as two python dictionaries with:
+                - keys: "dW1", "db1", ..., "dWL", "dbL" 
+                - values: numpy arrays of zeros of the same shape as the corresponding gradients/parameters.
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters.
+                    parameters["W" + str(l)] = Wl
+                    parameters["b" + str(l)] = bl
+    
+    Returns: 
+    v -- python dictionary that will contain the exponentially weighted average of the gradient.
+                    v["dW" + str(l)] = ...
+                    v["db" + str(l)] = ...
+    s -- python dictionary that will contain the exponentially weighted average of the squared gradient.
+                    s["dW" + str(l)] = ...
+                    s["db" + str(l)] = ...
+
+    """
+    # 由上面的Arguments 中的 对 parameters 描述可知，parameters 字典中包含所有参数，
+    # 参数有 w and  b 两种，所以 //2 取小的整数 （7 //2 = 3 , -3 //2 = -2）
+    
+    L = len(parameters) // 2 # number of layers in the neural networks
+    v = {}
+    s = {}
+    
+    # Initialize v, s. Input: "parameters". Outputs: "v, s".
+    for l in range(L):
+    ### START CODE HERE ### (approx. 4 lines)
+        v["dW" + str(l+1)] = np.zeros(parameters["W" + str(l+1)].shape)
+        v["db" + str(l+1)] = np.zeros(parameters["b" + str(l+1)].shape)
+        s["dW" + str(l+1)] = np.zeros(parameters["W" + str(l+1)].shape)
+        s["db" + str(l+1)] = np.zeros(parameters["b" + str(l+1)].shape)
+    ### END CODE HERE ###
+    
+    return v, s
+
+'''
+Adam 核心算法
+
+'''
+def update_parameters_with_adam(parameters, grads, v, s, t, learning_rate = 0.01, beta1 = 0.9, beta2 = 0.09, epsilon = 1e-8):
+    
+    L = len(parameters)//2 # 神经网络的层数
+    v_corrected = {}       # 初始化第一时刻的估计
+    s_corrected = {}       # 初始化第二时刻的估计 
+
+    # 更新所有层的参数 for loop 
+
+    for l in range(L):
+
+        # V_dw
+        v["dW" + str(l+1)] = beta1 * v["dW" + str(l+1)] + (1 - beta1) * grads["dW" + str(l+1)]
+
+		# V_db 
+
+        v["db" + str(l+1)] = beta1 * v["db" + str(l+1)] + (1 - beta1) * grads["db" + str(l+1)]
+
+		# V_corrected dw bias-corrected 偏差修正
+        v_corrected["dW" + str(l+1)] = v["dW" + str(l+1)]/(1 - beta1**t) 
+
+	    # V_corrected db
+        v_corrected["db" + str(l+1)] = v["db" + str(l+1)]/(1 - beta1**t)
+
+		# S_dw
+        s["dW" + str(l+1)] = beta2 * s["dW" + str(l+1)] + (1 - beta2)*(grads["dW" + str(l+1)]**2)
+
+		# S_db
+        s["db" + str(l+1)] = beta2 * s["db" + str(l+1)] + (1 - beta2)*(grads["db"] + str(l+1)**2)
+
+		# S_corrected dw
+        s_corrected["dW" +str(l+1)] = s_corrected["dW" + str(l+1)]/(1 - beta2**t)
+
+		# S_corrected db
+        s_corrected["db" + str(l+1)] = s_corrected["db" + str(l+1)]/(1 - beta2**t)
+
+        # W update
+        parameters["W" + str(l+1)] = parameters["W" + str(l+1)] - learning_rate * (v_corrected["dW" + str(l+1)]/(np.sqrt(s_corrected["dW" + str(l+1)]) + epsilon ))
+
+
+        # b update
+        parameters["b" + str(l+1)] = parameters["b" + str(l+1)] - learning_rate * (v_corrected["db" + str(l+1)]/(np.sqrt(s_corrected["db" + str(l+1)]) + epsilon ))
+
+    return parameters, v, s
+
+
+    
+
+if __name__ == '__main__':
+    # parameters = initialize_adam_test_case()
+
+    # v, s = initialize_adam(parameters)
+
+    parameters, grads, v, s = update_parameters_with_adam_test_case()
+    parameters, v, s  = update_parameters_with_adam(parameters, grads, v, s, t = 2)
+
+    print(v,"\n",s)
